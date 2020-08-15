a2ab895fa39912513fa119087c1c89cf56d13b11
nntd22121999 <nntd22121999@gmail.com>
2020-08-16 05:32:17 +0700

    last

--- mockapi.js
+++ mockapi.js
@@ -1,18 +1,51 @@
-
+let input_name = document.getElementById("input_name");
+let input_email = document.getElementById("input_email");
+let input_mess = document.getElementById("input_mess");
+let send_mess = document.getElementById("send_mess");
+let timenow = new Date();
+let timeString = dayjs(timenow).format('DD/MM/YYYY:HH:mm');
+console.log(timeString.value);
 //get
 async function demoApiGetAll (){
-    const response = await fetch('https://5f37ee6ebbfd1e00160bf74a.mockapi.io/api/login');
+    const response = await fetch('https://5f37ee6ebbfd1e00160bf74a.mockapi.io/api/contact');
     const data = await response.json();
     console.log(data);
 };
 
 demoApiGetAll();
 
+const contactApiPost = async () => {
+    const response = await fetch('https://5f37ee6ebbfd1e00160bf74a.mockapi.io/api/contact',
+    {
+        method:'POST',
+        headers:{
+            Accept:'application/json',
+            'Content-Type':'application/json',
+        },
+        body: JSON.stringify({
+            ten:input_name.value,
+            email:input_email.value,
+            mess:input_mess.value,
+            time:timeString
+        }),
+    });
+    const content = await response.json();
+    alert('Send message success');
+    input_name.value='';
+    input_email.value='';
+    input_mess.value='';
+};
+
+send_mess.addEventListener("click",() => contactApiPost());
+console.log(send_mess);
+
 // create 
+
+// put 
 // const demoApiPost = async () => {
-//     const response = await fetch('https://5f37ee6ebbfd1e00160bf74a.mockapi.io/api/login',
+//     const response = await fetch('https://5f37ee6ebbfd1e00160bf74a.mockapi.io/api/login/id',
 //     {
-//         method:'POST',
+//         method:'PUT',
 //         headers:{
 //             Accept:'application/json',
 //             'Content-Type':'application/json',
@@ -26,3 +59,4 @@ demoApiGetAll();
 //     console.log(content);
 // };
 // demoApiPost();
+
--- quantri.js
+++ quantri.js
@@ -1,8 +1,47 @@
 let table = document.getElementById("table");
+
+const deleteApi = async (a) => {
+    console.log(a);
+    const response = await fetch(`https://5f37ee6ebbfd1e00160bf74a.mockapi.io/api/contact/${a}`,
+    {
+        method:'DELETE',
+        headers:{
+            Accept:'application/json',
+            'Content-Type':'application/json',
+        }
+    });
+    const content = await response.json();
+    console.log(content);
+};
+
+async function demoApiGetAll (){
+    const response = await fetch('https://5f37ee6ebbfd1e00160bf74a.mockapi.io/api/contact');
+    const data = await response.json();
+    console.log(data);
+    for(let i =0 ; i< data.length ; i++){
+        table.innerHTML+=` <tr>
+        <td>${i+1}</td>
+        <td>${data[i].ten}</td>
+        <td>${data[i].email}</td>
+        <td>${data[i].mess}</td>
+        <td>${data[i].time}</td>
+        <td>
+            <a class = "tick" href="#" id="delete_${data[i].id}">&#10003</a>
+            <a href="#">&#10007</a>
+        </td>
+    </tr>`
+    // console.log(document.getElementById(`delete_${data[i].id}`));
+    // document.getElementById(`delete_${data[i].id}`).addEventListener("click",() => deleteApi(data[i].id));
+    }
+};
+demoApiGetAll();
+
 table.addEventListener("click",function(e){
     if (e.target.classList.contains("tick")) {
         if(confirm("Are you sure?")) {
             e.target.parentElement.parentElement.remove()
+            deleteApi(Number(e.target.id.split("_")[1]));
         }
     }
 });
+
